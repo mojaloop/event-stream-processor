1cb0d9f5b20104d1351447ce2d99a5a7a75d5c59
Valentin Genev <vgenev@gmail.com>
2019-08-23 15:41:03 +0300

    fixed tags, flags and errors

--- src/observables/apmTracerObservable.js
+++ src/observables/apmTracerObservable.js
@@ -5,21 +5,28 @@ const { tracer } = require('../lib/tracer')
 
 const apmTracerObservable = ({ message }) => {
   return Rx.Observable.create(observable => {
-    const flags = Buffer.alloc(1).fill(1)
+    const { service, traceId, parentSpanId, spanId, startTimestamp, finishTimestamp, flags, tags } = message.value.metadata.trace
+    const { status, code, description } = message.value.metadata.state
     const version = Buffer.alloc(1).fill(0)
-    const { service, traceId, parentSpanId, spanId, startTimestamp, finishTimestamp } = message.value.metadata.trace
+    const flagsBuffer = Buffer.alloc(1).fill(flags | 0x01)
     const traceIdBuff = Buffer.from(traceId, 'hex')
     const spanIdBuff = Buffer.from(spanId, 'hex')
     const parentSpanIdBuff = parentSpanId && Buffer.from(parentSpanId, 'hex')
-    Logger.info(`version: ${version.toString('hex')} traceId: ${traceId} spanId: ${spanId} parentSpanId: ${parentSpanId} flags: ${flags.toString('hex')}`)
+    Logger.info(`version: ${version.toString('hex')} traceId: ${traceId} spanId: ${spanId} parentSpanId: ${parentSpanId} flags: ${flagsBuffer.toString('hex')}`)
     const context = parentSpanIdBuff
-      ? new TraceParent(Buffer.concat([version, traceIdBuff, spanIdBuff, flags, parentSpanIdBuff]))
-      : new TraceParent(Buffer.concat([version, traceIdBuff, spanIdBuff, flags]))
+      ? new TraceParent(Buffer.concat([version, traceIdBuff, spanIdBuff, flagsBuffer, parentSpanIdBuff]))
+      : new TraceParent(Buffer.concat([version, traceIdBuff, spanIdBuff, flagsBuffer]))
     let span = tracer.startSpan(`${service}`, { startTime: Date.parse(startTimestamp) }, context)
-    tracer.startSpan()
-// the tags should be set by event sdk and just reassigned here if necessery
-    // span.setTag('payload', message.value.content.payload)
-    // span.setTag('transactionId', message.value.id)
+    if (tags) {
+      for (let [key, value] of Object.entries(tags)) {
+        span.setTag(key, value)
+      }
+    }
+    if (status === 'failed') {  // TODO add the enums from EventSDK
+      span.setTag('error', true)
+      !!code && span.setTag('errorCode', code)
+      !!description && span.setTag('errorDescription', description)
+    }
     span.finish(Date.parse(finishTimestamp))
     observable.next({ span })
   })
