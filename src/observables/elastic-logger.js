ee83d5020949bf494fcc3603dc4f068e6632d700
Miguel de Barros <miguel@debarros.me>
2019-09-06 15:46:11 +0100

    bump to version

--- src/observables/elastic-logger.js
+++ src/observables/elastic-logger.js
@@ -1,5 +1,6 @@
 const Rx = require('rxjs')
 const { client } = require('../lib/efk').elasticSearchClient
+const Logger = require('@mojaloop/central-services-shared').Logger
 
 const elasticsearchClientObservable = ({ message }) => {
   return Rx.Observable.create(async observable => {
@@ -11,6 +12,7 @@ const elasticsearchClientObservable = ({ message }) => {
       observable.complete()
     } catch (e) {
       observable.error(e)
+      Logger.error(e)
     }
   })
 }
