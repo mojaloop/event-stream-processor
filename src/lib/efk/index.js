517347608a970595908b8475118599ea0bfd76e8
Miguel de Barros <miguel@debarros.me>
2019-09-06 17:08:43 +0100

    updated to efk client
    - included template and getIndex from the efkClient
    - updated config for efk index

--- src/lib/efk/index.js
+++ src/lib/efk/index.js
@@ -1,6 +1,42 @@
+/*****
+ * @file This registers all handlers for the central-ledger API
+ License
+ --------------
+ Copyright © 2017 Bill & Melinda Gates Foundation
+ The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+ Contributors
+ --------------
+ This is the official list of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+
+ * Gates Foundation
+ - Name Surname <name.surname@gatesfoundation.com>
+
+ * ModusBox
+ - Valentin Genev <valentin.genev@modusbox.com>
+ - Miguel de Barros <miguel.debarros@modusbox.com>
+
+ --------------
+ ******/
+
+'use strict'
+
 const flogger = require('fluent-logger')
 const Config = require('../../lib/config')
 const Logger = require('@mojaloop/central-services-shared').Logger
+const Mustache = require('mustache')
+const Moment = require('moment')
 
 const configuration = Config.util.toObject()
 
@@ -18,8 +54,9 @@ const initLogger = async (prefix, options) => {
 
 const elasticsearch = require('elasticsearch')
 
-const ElasticSearchClient = (function () {
+const ElasticSearchClient = (() => {
   let instance
+  let index
   const createInstance = async () => {
     try {
       const client = new elasticsearch.Client({
@@ -39,6 +76,17 @@ const ElasticSearchClient = (function () {
   }
 
   return {
+    getIndex: () => {
+      if (!index) {
+        const indexConfig = configuration.efkClient.index
+        const dateString = Moment().format('YYYY.MM.DD')
+        index = Mustache.render(indexConfig.template, {
+          index: indexConfig.name,
+          date: dateString
+        })
+      }
+      return index
+    },
     getInstance: async () => {
       if (!instance) {
         instance = await createInstance()
