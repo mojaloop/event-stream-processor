1d38d5aba1ad1c4b98290408388f2b3b477600b8
Valentin Genev <vgenev@gmail.com>
2019-10-19 17:51:51 +0300

    update apm observable to move last span check into cache trace

--- src/observables/apmTracerObservable.js
+++ src/observables/apmTracerObservable.js
@@ -1,16 +1,13 @@
 const Rx = require('rxjs')
 const Logger = require('@mojaloop/central-services-logger')
 const { Tracer } = require('@mojaloop/event-sdk')
-const { cacheSpanContext, createTrace } = require('../lib/masterSpan')
+const { cacheSpanContext } = require('../lib/masterSpan')
 
 const apmTracerObservable = ({ message }) => {
   return Rx.Observable.create(async observable => {
     try {
       const spanCtx = Tracer.extractContextFromMessage(message.value)
       await cacheSpanContext(spanCtx, message.value.metadata.event.state, message.value.content)
-      if (spanCtx.service === 'final service') {
-        await createTrace(spanCtx.traceId)
-      }
       observable.next(true)
     } catch (e) {
       Logger.error(e)
